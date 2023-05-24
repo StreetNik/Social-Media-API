114685e6e2187d890031ecc4913ee47149c7f04d
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-05-23 23:16:20 -0500

    Add endpoint for token obtain

--- user/urls.py
+++ user/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 
 from rest_framework.authtoken import views
 
-from user.views import CreateUserView
+from user.views import CreateUserView, CustomAuthToken
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
-    path("token/", views.obtain_auth_token, name="token"),
+    path("token/", CustomAuthToken.as_view(), name="token"),
 ]
 
 app_name = "user"
--- user/views.py
+++ user/views.py
@@ -1,6 +1,20 @@
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, CustomAuthTokenSerializer
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = CustomAuthTokenSerializer(
+            data=request.data, context={"request": request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key})
 
 
 class CreateUserView(generics.CreateAPIView):
