44889c24ff6056ba38aaf8737e920929e895d249
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-05-25 15:57:23 -0500

    Add profile model and update migrations

--- user/models.py
+++ user/models.py
@@ -1,6 +1,10 @@
+import os
+import uuid
+
 from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 
 
@@ -41,3 +45,41 @@ class User(AbstractUser):
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+
+def profile_image_file_path(instance, filename: str):
+    email = instance.email.split("@")[0]
+    directory = email[0]
+    _, extension = os.path.splitext(filename)
+
+    filename = f"{slugify(email)}-{uuid.uuid4()}.{extension}"
+
+    return os.path.join("uploads/prof-pic/", directory, filename)
+
+
+MARRIAGE_STATUSES = (
+    ("none", ""),
+    ("single", "Single"),
+    ("married", "Married"),
+    ("relationship", "In relationship"),
+    ("looking", "Looking for someone"),
+)
+
+SEX_LIST = (
+    ("none", ""),
+    ("men", "Men"),
+    ("women", "Women"),
+)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to=profile_image_file_path)
+    bio = models.CharField(max_length=255)
+    marriage_status = models.CharField(
+        max_length=255, choices=MARRIAGE_STATUSES, default="none"
+    )
+    sex = models.CharField(max_length=50, choices=SEX_LIST, default="none")
+
+    followers = models.ManyToManyField(User, related_name="following", blank=True)
+    following = models.ManyToManyField(User, related_name="followers", blank=True)
--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from user.views import CreateUserView, CreateTokenView, LogOutView
@@ -6,6 +8,6 @@ urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
     path("token/", CreateTokenView.as_view(), name="token"),
     path("logout/", LogOutView.as_view(), name="logout"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 app_name = "user"
