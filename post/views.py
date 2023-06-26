27c4ec805755c581795e786148789f94490a7584
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-06-26 10:58:23 -0500

    Add 2 endpoints for retrieve, create and update posts

--- post/views.py
+++ post/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from post.models import Post
+from post.serializers import PostListSerializer, PostDetailSerializer
+
+
+class PostListViewSet(ModelViewSet):
+    serializer_class = PostListSerializer
+    queryset = Post.objects.all()
+
+
+class PostDetailView(generics.RetrieveUpdateAPIView):
+    serializer_class = PostDetailSerializer
+
+    def get_object(self):
+        post = Post.objects.get(pk=self.kwargs["pk"])
+        return post
