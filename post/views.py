092e2d2410cdd4af0d60201426c9158cb33faf8e
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-06-29 21:37:01 -0500

    Add toggle like function for post viewset

--- post/views.py
+++ post/views.py
@@ -1,18 +1,35 @@
-from rest_framework import generics
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 
 from post.models import Post
 from post.serializers import PostListSerializer, PostDetailSerializer
 
 
-class PostListViewSet(ModelViewSet):
-    serializer_class = PostListSerializer
+class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
 
+    def get_serializer_class(self):
+        if self.action != 'detail':
+            return PostListSerializer
+        return PostDetailSerializer
 
-class PostDetailView(generics.RetrieveUpdateAPIView):
-    serializer_class = PostDetailSerializer
+    @action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
+    def toggle_like(self, request, pk=None):
+        post = self.get_object()
 
-    def get_object(self):
-        post = Post.objects.get(pk=self.kwargs["pk"])
-        return post
+        # Get the authenticated user
+        user = request.user
+
+        if user in post.people_who_liked.all():
+            # User already liked the post, remove the like
+            post.people_who_liked.remove(user)
+            message = "Post unliked successfully."
+        else:
+            # User has not liked the post, add the like
+            post.people_who_liked.add(user)
+            message = "Post liked successfully."
+
+        return Response({"message": message}, status=status.HTTP_200_OK)
