7a6aba6c3a2902339efb72df7730d9ef3bb3267f
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-07-25 21:03:15 -0500

    Add ability to create comments and filter post list(All posts, following users posts only)

--- post/views.py
+++ post/views.py
@@ -4,17 +4,43 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 
-from post.models import Post
-from post.serializers import PostListSerializer, PostDetailSerializer
+from post.models import Post, PostComment
+from post.serializers import PostListSerializer, PostDetailSerializer, CommentSerializer
 
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
 
+    def get_queryset(self):
+        following_only = self.request.GET.get("following-only", None)
+        user = self.request.user
+
+        if following_only == "True" and user.is_authenticated:
+            following_users = user.profile.following.all()
+            return Post.objects.filter(user__in=following_users)
+
+        return Post.objects.all()
+
     def get_serializer_class(self):
         if self.action != 'detail':
             return PostListSerializer
         return PostDetailSerializer
+
+    @action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
+    def create_comment(self, request, pk=None):
+        post = self.get_object()
+        user = request.user
+        comment_data = request.data.get("comment")
+
+        comment = PostComment.objects.create(user=user, post=post, content=comment_data)
+
+        return Response(
+            {
+                "message": "Comment created successfully.",
+                "comment_id": comment.id
+            },
+            status=status.HTTP_201_CREATED
+        )
 
     @action(detail=True, methods=["post"], permission_classes=[IsAuthenticated])
     def toggle_like(self, request, pk=None):
