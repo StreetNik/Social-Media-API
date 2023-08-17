79fd712ec65297a9865dfe4c5a6b0c2758bd32ca
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-08-16 19:19:18 -0500

    Optimized urls file

--- post/urls.py
+++ post/urls.py
@@ -10,18 +10,6 @@ router = routers.DefaultRouter()
 
 router.register(r"post-list", PostViewSet, basename="post-detail")
 
-urlpatterns = [
-    path("", include(router.urls)),
-    path(
-        "post-list/<int:pk>/like-toggle/",
-        PostViewSet.as_view({"post": "toggle_like"}),
-        name="post-toggle-like",
-    ),
-    path(
-        "post-list/<int:pk>/new-comment/",
-        PostViewSet.as_view({"post": "create_comment"}),
-        name="post-new-comment",
-    ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [] + router.urls + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 app_name = "post"
