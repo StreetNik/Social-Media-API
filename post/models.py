3af95c577ea2108392349ddcf2d8a4b43626001b
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2023-08-16 19:14:03 -0500

    Fixed extension error and file path in profile_picture_file_path function

--- post/models.py
+++ post/models.py
@@ -31,9 +31,9 @@ def profile_picture_file_path(instance, filename: str):
     directory = title[0]
     _, extension = os.path.splitext(filename)
 
-    filename = f"{slugify(title)}-{uuid.uuid4()}.{extension}"
+    filename = f"{slugify(title)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/prof-pic/", directory, filename)
+    return os.path.join("uploads", "prof-pic", directory, filename)
 
 
 class PostImage(models.Model):
